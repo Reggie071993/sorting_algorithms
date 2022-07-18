35a33caafbc9484de87666f615cf50dec6533fb9
Reggie071993 <reykakwela@yahoo.com>
2022-07-18 19:23:43 +0000

    105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,133 +1,76 @@
 #include "sort.h"
-#include <stdlib.h>
 
 /**
- * init_bucket_count - resets bucket_count values to 0
- * @bucket_count: array containing amounts of members for arrays in `buckets`
- */
-void init_bucket_count(int *bucket_count)
-{
-	int i;
+ * radix_sort - sort array with radix method
+ * @array: array to sort
+ * @size: size of the array
+ *
+ * Return: nothing
+*/
 
-	for (i = 0; i < 10; i++)
-		bucket_count[i] = 0;
-}
-
-/**
- * build_buckets - allocates space for arrays to be held in `buckets`
- * @buckets: array of arrays each containing sorted members of `array`
- * @bucket_count: array containing amounts of members for arrays in `buckets`
- */
-void build_buckets(int *bucket_count, int **buckets)
-{
-	int *bucket;
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		bucket = malloc(sizeof(int) * bucket_count[i]);
-		if (!bucket)
-		{
-			for (; i > -1; i--)
-				free(buckets[i]);
-			free(buckets);
-			exit(EXIT_FAILURE);
-		}
-		buckets[i] = bucket;
-	}
-	init_bucket_count(bucket_count);
-}
-
-/**
- * find_max - searches array of integers for highest value
- * @array: array of values to be searched
- * @size: number of elements in array
- * Return: largest integer stored in array
- */
-int find_max(int *array, size_t size)
-{
-	int max;
-	size_t i;
-
-	max = array[0];
-	for (i = 1; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
-	return (max);
-}
-
-/**
- * into_array - flattens buckets into array sorted by current digit place,
- * then prints results and frees current buckets for next pass
- * @array: array of values to be printed
- * @size: number of elements in array
- * @buckets: array of arrays each containing sorted members of `array`
- * @bucket_count: array containing amounts of members for arrays in `buckets`
- */
-void into_array(int *array, size_t size, int **buckets, int *bucket_count)
-{
-	int i, j, k;
-
-	/* flatten bucket members in order into array sorted by place */
-	for (k = 0, i = 0; k < 10; k++)
-	{
-		for (j = 0; j < bucket_count[k]; j++, i++)
-			array[i] = buckets[k][j];
-	}
-	/* print results */
-	print_array(array, size);
-	/* free buckets from current pass */
-	for (i = 0; i < 10; i++)
-		free(buckets[i]);
-}
-
-/**
- * radix_sort - Sorts array of integers in ascending order using a Radix sort
- * alogrithm starting with the LSD, the 'least significant (1s place) digit',
- * and sorting by next digit to left. Size of `bucket_count` here determined
- * by max range of key variance (digits 0-9), other solutions may be needed for
- * much larger ranges.
- * @array: array of values to be sorted
- * @size: number of elements in array
- */
-{
-	int **buckets;
 void radix_sort(int *array, size_t size)
-	int bucket_count[10];
-	int max, max_digits, pass, divisor, digit;
-	size_t i;
+{
+	int i, j, x, y, cantRep, max, div = 1, t = 0;
+	int buckets[10][1000];
 
 	if (!array || size < 2)
 		return;
-	buckets = malloc(sizeof(int *) * 10);
-	if (!buckets)
-		exit(1);
-	/* find amount of places in largest element */
-	max = find_max(array, size);
-	for (max_digits = 0; max > 0; max_digits++)
-		max /= 10;
-	/* one sorting pass for each place in max_digits */
-	for (pass = 0, divisor = 1; pass < max_digits; pass++, divisor *= 10)
+
+	max = array[0];
+	for (i = 1; i < (int) size; i++)
+		if (array[i] > max)
+			max = array[i];
+
+	for (i = 0; i < 10; ++i)
+		for (j = 0; j < 1000; ++j)
+			buckets[i][j] = -1;
+
+	cantRep = getCantRep(max);
+
+	for (i = 0; i < cantRep; ++i)
 	{
-		init_bucket_count(bucket_count);
-		/* find amount of members in each bucket */
-		for (i = 0; i < size; i++)
+		for (j = 0; j < (int) size; ++j)
 		{
-			digit = (array[i] / divisor) % 10;
-			bucket_count[digit]++;
+			for (y = 0; buckets[(array[j] / div) % 10][y] != -1; y++)
+				;
+			buckets[(array[j] / div) % 10][y] = array[j];
 		}
-		build_buckets(bucket_count, buckets);
-		/* fill buckets sorting by digit at current power of 10 */
-		for (i = 0; i < size; i++)
+		div = div * 10;
+		t = 0;
+		for (x = 0; x < 10; ++x)
 		{
-			/* find digit of source element at that power of 10 */
-			digit = (array[i] / divisor) % 10;
-			/* place member of source array in digit's bucket */
-			buckets[digit][bucket_count[digit]] = array[i];
-			/* record increase in bucket fill level */
-			bucket_count[digit]++;
+			for (y = 0; buckets[x][y] != -1; y++)
+			{
+				array[t] = buckets[x][y];
+				buckets[x][y] = -1;
+				t++;
+			}
 		}
-		into_array(array, size, buckets, bucket_count);
+		print_array(array, size);
 	}
-	free(buckets);
+}
+
+/**
+ * getCantRep - Returns the number of digits of the largest number in the array
+ * @num: The largest number
+ *
+ * Return: Number of digits of the num
+*/
+
+int getCantRep(int num)
+{
+	bool flag = true;
+	int cont = 0;
+
+	while (flag)
+	{
+		flag = false;
+		if (num > 0)
+		{
+			num = num / 10;
+			cont++;
+			flag = true;
+		}
+	}
+	return (cont);
 }
